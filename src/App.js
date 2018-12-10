451b1bbd7e65b197b6e39081d2504d6a28348edc
ErnieBuyco <ernie.buyco@fmclarity.com>
2018-12-10 19:29:20 +0800

    api_updates

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import axios from 'axios';
+
+import './App.css';
+import ImgList from './Components/ImgList';
+import SearchForm from './Components/SearchForm';
+
+
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			imgs: [],
+			loadingState: true
+		};
+	}
+
+	componentDidMount() {
+		this.performSearch();
+	}
+
+	performSearch = (query = 'sun') => {
+		axios
+			.get(
+				`https://api.unsplash.com/search/photos?page=1&query=kittens&client_id=b63847891c51c5247e92b4687bcb4ad0224ddf8cbb3908468c4f69495756d365`
+			)
+			.then(data => {
+				this.setState({ imgs: data.data.results, loadingState: false });
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="main-header">
+					<div className="inner">
+						<h1 className="main-title">ImageSearch</h1>
+						<SearchForm onSearch={this.performSearch} />
+					</div>
+				</div>
+				<div className="main-content">
+					{this.state.loadingState
+						? <p>Loading</p>
+						: <ImgList data={this.state.imgs} />}
+				</div>
+			</div>
+		);
+	}
+}
